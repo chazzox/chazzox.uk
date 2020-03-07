191410f28c234c03f5a2730f24739338912945ae
charlie <42206116+chazzox@users.noreply.github.com>
2020-03-07 14:50:53 +0000

    added boundary detection, fixed escape button on index page

--- assets/battleships/scripts/script.js
+++ assets/battleships/scripts/script.js
@@ -1,62 +1,77 @@
-let len = 10,
-	nodes = new Array(len),
-	battle_len = document.getElementById('');
-positions = new Array();
-
+let len = 10;
+let nodes = new Array(len);
+let battle_len = 2;
+let positions = new Array();
+let mode = false;
 let sketch = function(p) {
+	// only ran when page loads
 	p.setup = function() {
 		positions.push([1, 1]);
 		p.background(20);
 		c = p.color(107);
 		let width = Math.round(p.displayHeight * (5 / 80)) * 10;
 		p.createCanvas(width, width);
-		for (let i = 0; i < len; i++) {
-			nodes[i] = new Array(len);
-			for (let j = 0; j < len; j++) {
-				nodes[i][j] = new square((j / len) * p.width, (i / len) * p.height, c);
-				nodes[i][j].show(c);
+		for (let y = 0; y < len; y++) {
+			nodes[y] = new Array(len);
+			for (let x = 0; x < len; x++) {
+				nodes[y][x] = new square((x / len) * p.width, (y / len) * p.height, c);
+				nodes[y][x].show(c);
 			}
 		}
 	};
 
+	// ran many times a second
 	p.draw = function() {
-		for (var i = 0; i < nodes.length; i++) {
-			for (var j = 0; j < nodes.length; j++) {
-				// nodes[i][j].show(c);
+		for (var y = 0; y < nodes.length; y++) {
+			for (var x = 0; x < nodes.length; x++) {
+				// looping through every equare
+
+				//checking if mouse hovering over a given square
 				if (
-					p.mouseX > nodes[i][j].x &&
-					p.mouseX < nodes[i][j].x + p.width / 10 &&
-					p.mouseY > nodes[i][j].y &&
-					p.mouseY < nodes[i][j].y + p.width / 10
+					p.mouseX > nodes[y][x].x &&
+					p.mouseX < nodes[y][x].x + p.width / 10 &&
+					p.mouseY > nodes[y][x].y &&
+					p.mouseY < nodes[y][x].y + p.width / 10
 				) {
-					if (positions[positions.length - 1].x == nodes[i][j].x) {
-						positions.push([nodes[i][j].x, nodes[i][j].y]);
+					// if the square is different to the last, push new cords to positions array
+					// otherwise clean grid off
+					if (positions[positions.length - 1].x == nodes[y][x].x) {
+						positions.push([nodes[y][x].x, nodes[y][x].y]);
 					} else {
 						clear();
 					}
+					// drawing rectange where mosue is
 					p.fill(0);
 					p.rect(
-						nodes[i][j].x,
-						nodes[i][j].y,
-						(p.width / len) * battle_len,
-						p.width / len
+						nodes[y][x + (x == 9 && mode ? -1 : 0)].x,
+						nodes[y + (y == 9 && !mode ? -1 : 0)][x].y,
+						// if the mode is horizontal, then times the width by the length of the battleship
+						(p.width / len) * (mode ? battle_len : 1),
+						// same but 4 vertical
+						(p.width / len) * (mode ? 1 : battle_len)
 					);
 				}
 			}
 		}
 
+		// adding a new sqare to board
 		p.mouseClicked = function() {
-			for (var i = 0; i < nodes.length; i++) {
-				for (var j = 0; j < nodes.length; j++) {
-					nodes[i][j].show(c);
+			for (var y = 0; y < nodes.length; y++) {
+				for (var x = 0; x < nodes.length; x++) {
+					nodes[y][x].show(c);
 					if (
-						p.mouseX > nodes[i][j].x &&
-						p.mouseX < nodes[i][j].x + p.width / 10 &&
-						p.mouseY > nodes[i][j].y &&
-						p.mouseY < nodes[i][j].y + p.width / 10
+						p.mouseX > nodes[y][x].x &&
+						p.mouseX < nodes[y][x].x + p.width / 10 &&
+						p.mouseY > nodes[y][x].y &&
+						p.mouseY < nodes[y][x].y + p.width / 10
 					) {
 						for (var shipLen = 0; shipLen < battle_len; shipLen++) {
-							nodes[i][j + shipLen].boolPressed = !nodes[i][j + shipLen].boolPressed;
+							// not entierly sure
+							let selectorNode =
+								nodes[y + (mode ? 0 : shipLen) + (y == 9 && !mode ? -1 : 0)][
+									x + (mode ? shipLen : 0) + (x == 9 && mode ? -1 : 0)
+								];
+							selectorNode.boolPressed = !selectorNode.boolPressed;
 						}
 					}
 				}
@@ -72,7 +87,7 @@ let sketch = function(p) {
 			this.boolPressed = false;
 		}
 		show() {
-			p.fill(this.boolPressed ? 0 : 107);
+			p.fill(this.boolPressed ? 0 : c);
 			p.rect(this.x, this.y, p.width / len, p.width / len);
 		}
 	}
@@ -82,16 +97,21 @@ let sketch = function(p) {
 	};
 };
 function clear() {
-	for (var i = 0; i < nodes.length; i++) {
-		for (var j = 0; j < nodes.length; j++) {
-			nodes[i][j].show(c);
+	for (var y = 0; y < nodes.length; y++) {
+		for (var x = 0; x < nodes.length; x++) {
+			nodes[y][x].show(c);
 		}
 	}
 }
 function un_check(x) {
-	for (var i = 0; i < x.parentNode.children.length; i++) {
-		if (i % 2 == 0) {
-			x.parentNode.children[i].checked = false;
+	for (var y = 0; y < x.parentNode.children.length; y++) {
+		if (x.id.slice(6, -1) == '0') {
+			mode = x.id[x.id.length - 1] == 1;
+		} else if (x.id.slice(6, -1) == '1') {
+			battle_len = x.id[x.id.length - 1];
+		}
+		if (y % 2 == 0) {
+			x.parentNode.children[y].checked = false;
 		}
 	}
 	x.checked = true;
--- assets/homepage/scripts/main.js
+++ assets/homepage/scripts/main.js
@@ -1,21 +1,34 @@
+dispButton = document.getElementById('proj');
+dispButton.onclick = function() {
+	document.getElementById('noShow').style.display = 'block';
+	document.getElementById('closeButton').style.display = 'block';
+	document.getElementById('main').style.filter = 'blur(7px)';
+	for (var i = 0; i < document.getElementsByClassName('boxforlink').length; i++) {
+		document.getElementsByClassName('boxforlink')[i].style.padding = '20px';
+	}
+};
 
-
-dispButton = document.getElementById("proj")
-dispButton.onclick = function(){
-    document.getElementById("noShow").style.display = "block"
-    document.getElementById("closeButton").style.display = "block"
-    document.getElementById("main").style.filter= "blur(7px)"
-    for(var i=0;i<document.getElementsByClassName("boxforlink").length();i++){
-    document.getElementsByClassName("boxforlink")[i].style.padding = "20px"
-    }
+closeButt = document.getElementById('closeButton');
+function close() {
+	document.getElementById('noShow').style.display = 'none';
+	document.getElementById('closeButton').style.display = 'none';
+	document.getElementById('main').style.filter = 'blur(0px)';
+	for (var i = 0; i < document.getElementsByClassName('boxforlink').length; i++) {
+		document.getElementsByClassName('boxforlink')[i].style.padding = '';
+	}
 }
 
-closeButt = document.getElementById("closeButton")
-closeButt.onclick = function(){
-    document.getElementById("noShow").style.display = "none"
-    document.getElementById("closeButton").style.display = "none"
-    document.getElementById("main").style.filter = "blur(0px)"
-    for(var i=0;i<document.getElementsByClassName("boxforlink").length();i++){
-    document.getElementsByClassName("boxforlink")[i].style.padding = ""
-    }
-}
+closeButt.onclick = () => close();
+document.onkeydown = function(evt) {
+	evt = evt || window.event;
+	var isEscape = false;
+	if ('key' in evt) {
+		isEscape = evt.key === 'Escape' || evt.key === 'Esc';
+	} else {
+		isEscape = evt.keyCode === 27;
+	}
+	if (isEscape) {
+		console.log('bruh');
+		close();
+	}
+};
