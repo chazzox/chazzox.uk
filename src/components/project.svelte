c4f8f411e0e6c028faa56cf5c6d961c89d8175e8
chazzox <caylott@outlook.com>
2022-09-26 04:29:00 +0100

    feat: improved project link dom structure

--- src/components/project.svelte
+++ src/components/project.svelte
@@ -5,9 +5,7 @@
 </script>
 
 <div class="projectContainer my-4">
-	<p class="py-1 hidden sm:block">
-		<a href={url} target="_blank" class="underline">{name}</a> - {description}
+	<p class="sm:py-1">
+		<a href={url} target="_blank" class="underline text-lg sm:text-base">{name}</a><span class="hidden sm:inline-block whitespace-pre px-3">-</span><span class="block sm:inline-block">{description}</span>
 	</p>
-	<a href={url} target="_blank" class="underline text-lg sm:hidden text-center block">{name}</a>
-	<p class="sm:hidden text-center">{description}</p>
 </div>
