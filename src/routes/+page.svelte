7db33b9d0558191509a2d53a733c8491fc1ebc19
chazzox <caylott@outlook.com>
2022-10-12 14:37:25 +0100

    fix: removed backdrop blur hack around

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,6 @@
 <script>
-	import { onMount } from 'svelte';
-
 	import Footer from '../components/footer.svelte';
 	import Project from '../components/project.svelte';
-
-	let bgClassString = 'chrome';
-
-	onMount(async () => {
-		if (
-			!(
-				window.navigator.userAgent.includes('Chrome') ||
-				window.navigator.userAgent.includes('Safari')
-			)
-		) {
-			bgClassString = 'firefox';
-		}
-	});
 </script>
 
 <svelte:head>
@@ -29,15 +14,12 @@
 	<meta name="twitter:card" content="/images/screenshot.jpg" />
 </svelte:head>
 
-<div class="flex flex-col dark:text-white min-h-screen bg-cover bg-main-img bg-center bg-fixed">
-	<div class="flex-1 flex justify-center items-center">
-		<div
-			class={'border-[1px] border-gray-700 p-4 rounded-xl shadow-xl m-6 relative overflow-hidden bg-extends-white ' +
-				bgClassString}
-		>
+<div class="flex min-h-screen flex-col bg-main-img bg-cover bg-fixed bg-center dark:text-white">
+	<div class="flex flex-1 items-center justify-center">
+		<div class="relative m-6 overflow-hidden rounded-xl border-[1px] border-gray-700 p-4 shadow-xl backdrop-blur-lg">
 			<div class="relative z-50 text-center sm:text-left">
-				<h1 class="text-2xl font-bold my-3"><span class="hidden sm:inline"># </span>Charlie</h1>
-				<h2 class="text-lg sm:text-xl font-bold my-3">
+				<h1 class="my-3 text-2xl font-bold"><span class="hidden sm:inline"># </span>Charlie</h1>
+				<h2 class="my-3 text-lg font-bold sm:text-xl">
 					<span class="hidden sm:inline">## </span>Student/Full Stack Dev
 				</h2>
 				<p class="my-3">
@@ -45,20 +27,20 @@
 				</p>
 				<p class="my-3">
 					<span class="hidden sm:inline">- </span>I'm a full time Computer Science student @
-					<a class="underline font-semibold" href="https://www.royalholloway.ac.uk/">RHUL</a>
+					<a class="font-semibold underline" href="https://www.royalholloway.ac.uk/">RHUL</a>
 					while doing full Stack work on the side
 				</p>
 				<p class="my-3">
 					<span class="hidden sm:inline">- </span>Working part time for<a
-						class="underline pl-2 px-1 font-semibold"
+						class="px-1 pl-2 font-semibold underline"
 						href="https://projectwaitless.com/">Project Waitless</a
 					>
 					and
-					<a class="underline px-1 font-semibold" href="https://www.gosystem.co.uk/">goSystem</a>
+					<a class="px-1 font-semibold underline" href="https://www.gosystem.co.uk/">goSystem</a>
 				</p>
 				<p class="my-3">
-					<span class="hidden sm:inline">- </span>Below are some of my projects in varying states of
-					completion that i've tinkered with over the years<span class="hidden sm:inline">:</span>
+					<span class="hidden sm:inline">- </span>Below are some of my projects in varying states of completion
+					that i've tinkered with over the years<span class="hidden sm:inline">:</span>
 				</p>
 				<div class="sm:ml-3">
 					<Project
@@ -66,11 +48,7 @@
 						name="discordify"
 						description="A Betterdiscord plugin"
 					/>
-					<Project
-						url="https://hydrova.chazzox.uk/"
-						name="Hydrova"
-						description="High perfomance reddit client"
-					/>
+					<Project url="https://hydrova.chazzox.uk/" name="Hydrova" description="High perfomance reddit client" />
 					<Project
 						url="https://github.com/rhul-compsoc/twitter-stream/"
 						name="Twitter Stream"
@@ -81,21 +59,9 @@
 						name="Faster Panopto"
 						description="Browser extension for panopto lectures"
 					/>
-					<Project
-						url="https://shortcuts.chazzox.uk/"
-						name="Shortcuts"
-						description="Your bookmarks on one site"
-					/>
-					<Project
-						url="https://hack.chazzox.uk/"
-						name="oxHack"
-						description="My submission to oxford hack 22"
-					/>
-					<Project
-						url="https://replay.chazzox.uk/"
-						name="Replay"
-						description="A discover weekly backup tool"
-					/>
+					<Project url="https://shortcuts.chazzox.uk/" name="Shortcuts" description="Your bookmarks on one site" />
+					<Project url="https://hack.chazzox.uk/" name="oxHack" description="My submission to oxford hack 22" />
+					<Project url="https://replay.chazzox.uk/" name="Replay" description="A discover weekly backup tool" />
 					<Project
 						url="https://github.com/chazzox/BetterDiscordPlugins/"
 						name="BetterDiscordPlugins"
@@ -105,5 +71,5 @@
 			</div>
 		</div>
 	</div>
-	<Footer blurTechnique={bgClassString} />
+	<Footer />
 </div>
