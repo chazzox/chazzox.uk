05c4923ab2a75cb1710f2526121c678ee0cf07c6
charlie.aylott <charlie.aylott@matillion.com>
2024-05-26 02:34:40 +0100

    chore: upgraded versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/static";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   output: "static",
-  server: { port: 3000 },
+  server: {
+    port: 3000
+  },
   adapter: vercel({
     analytics: false
   })
