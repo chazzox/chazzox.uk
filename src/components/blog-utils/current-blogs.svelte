7a00a22ea007f5d67ec986df5e2d1b93048176f1
charlie.aylott <caylott@outlook.com>
2024-10-10 18:08:14 +0100

    feat(): progress commit

--- src/components/blog-utils/current-blogs.svelte
+++ src/components/blog-utils/current-blogs.svelte
@@ -7,9 +7,7 @@
 </script>
 
 <div class="flex flex-col gap-3 py-5">
-    {#each $posts.filter((p) => (p.data.tags || []).filter((v) => {
-                return $tags[v.id];
-            }).length > 0) as blog}
+    {#each $posts.filter((p) => (p.data.tags || []).filter((v) => $tags[v.id]).length > 0) as blog}
         <a
             href={`/blogs/${blog.slug}`}
             class="block max-w-2xl border-1 border-black p-3 shadow-lg transition-colors hover:bg-gray-300 dark:border-white dark:text-white dark:hover:bg-white dark:hover:text-black"
@@ -20,6 +18,6 @@
             <h4>author: {blog.data.author.id}</h4>
         </a>
     {:else}
-        <p>not much to see here...</p>
+        <p class="dark:text-white">not much to see here...</p>
     {/each}
 </div>
