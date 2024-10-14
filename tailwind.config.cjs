9257988023fa46fe8ae0ba6b505ddf63389d65e0
charlie.aylott <caylott@outlook.com>
2024-10-14 13:26:08 +0100

    feat: added theme toggle

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: [
-        "variant",
-        ["@media (prefers-color-scheme: dark) { &:not(.light *) }", "&:is(.dark *)"]
-    ],
+    darkMode: ["selector", '[data-theme="dark"]'],
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
